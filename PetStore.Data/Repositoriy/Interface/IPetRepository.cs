9070e1d0e330a5440639014f900f7a1ad93ff2fd
yannickvanhecke <yannick.vanhecke@icloud.com>
2025-06-04 17:12:03 +0200

    added interface methods for adding Pet and saving pets.

--- PetStore.Data/Repositoriy/Interface/IPetRepository.cs
+++ PetStore.Data/Repositoriy/Interface/IPetRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PetStore.Data.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace PetStore.Data.Repositoriy.Interface
 {
     public interface IPetRepository
     {
+        void Add(Pet pet);
+        void SaveChanges();
     }
 }
