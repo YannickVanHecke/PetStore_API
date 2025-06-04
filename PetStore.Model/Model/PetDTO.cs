31ba5fd12e35ee63c599b651be33a3ad35b168c8
yannickvanhecke <yannick.vanhecke@icloud.com>
2025-06-04 17:38:10 +0200

    changed animaltype to integer.

--- PetStore.Model/Model/PetDTO.cs
+++ PetStore.Model/Model/PetDTO.cs
@@ -11,7 +11,7 @@ namespace PetStore.Model.Model
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public PetKind AnimalType { get; set; }
+        public int AnimalType { get; set; }
         public DateTime BirthDate { get; set; }
         public bool Sex { get; set; }
     }
