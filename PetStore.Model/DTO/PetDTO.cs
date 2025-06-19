19c8a3f354b1e6accbfaeb226120572fabef4609
yannickvanhecke <yannick.vanhecke@icloud.com>
2025-06-19 17:24:47 +0200

    fixing folder naming

--- PetStore.Model/DTO/PetDTO.cs
+++ PetStore.Model/DTO/PetDTO.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PetStore.Model.Model
+namespace PetStore.Model.DTO
 {
     public class PetDTO
     {
---
+++ PetStore.Model/DTO/StatisticsDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore.Model.DTO
+{
+    public class StatisticsDTO
+    {
+        public List<string> Labels { get; set; }
+
+        public StatisticsDTO(IEnumerable<PetDTO> pets)
+        {
+            this.Labels = GetPetKind(pets);
+
+        }
+
+        private List<string> GetPetKind(IEnumerable<PetDTO> pets)
+        {
+            var labels = new List<string>();
+
+            foreach (var pet in pets)
+            {
+                if (!labels.Contains(pet.Name))
+                {
+                    labels.Add(pet.Name);
+                }
+            }
+
+            return labels;
+        }
+    }
+}
