8c96d1218057bdffd12e109a321c475d4f9c1ddb
yannickvanhecke <yannick.vanhecke@icloud.com>
2025-06-19 17:38:41 +0200

    creating data point class for return to front-end;

---
+++ PetStore.Model/DTO/DataPointDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore.Model.DTO
+{
+    public class DataPointDTO
+    {
+        public string X { get; set; }
+        public int Y { get; set; }
+    }
+}
