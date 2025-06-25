b6bda24d3c24ed149f64fe449c184e67af812062
yannickvanhecke <yannick.vanhecke@icloud.com>
2025-06-25 14:08:31 +0200

    mapping data in correct form for webclient.

--- PetStore.Model/DTO/StatisticsDTO.cs
+++ PetStore.Model/DTO/StatisticsDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PetStore.Model.DTO.ChartDTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,48 +9,16 @@ namespace PetStore.Model.DTO
 {
     public class StatisticsDTO
     {
-        public List<string> Labels { get; set; }
-        public Dictionary<int, int> Data { get; set; }
+        public List<ChartSerie> series { get; set; }
+        public List<XAxis> xaxis { get; set; }
 
         public StatisticsDTO(IEnumerable<PetDTO> pets)
         {
-            this.Labels = GetPetKind(pets);
-            this.Data = MapPets(pets);
-
+            var chartSerie = new ChartSerie("Overzicht van de huisdieren", pets);
+            this.series = [chartSerie];
+            this.xaxis = [new XAxis(chartSerie.Labels.ToList())];
         }
 
-        private List<string> GetPetKind(IEnumerable<PetDTO> pets)
-        {
-            var labels = new List<string>();
-
-            foreach (var pet in pets)
-            {
-                if (!labels.Contains(pet.Name))
-                {
-                    labels.Add(pet.Name);
-                }
-            }
-
-            return labels;
-        }
-
-        private Dictionary<int, int> MapPets(IEnumerable<PetDTO> pets) { 
-            var data = new Dictionary<int, int>();
-            foreach (var pet in pets)
-            {
-                if(!data.Keys.Contains(pet.AnimalType)) {
-
-                    data.Add(pet.AnimalType, 1);
-                }
-                else
-                {
-                    var numberOfPets = data[pet.AnimalType];
-                    numberOfPets++;
-                    data.Remove(pet.AnimalType);
-                    data.Add(pet.AnimalType, numberOfPets);
-                }
-            }
-            return data;
-        }
+        
     }
 }
